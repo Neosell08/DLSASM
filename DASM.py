5e1f16cfc4e83a436e6fb8b9b8dd672ba3a1307a
Neosell <141836762+Neosell08@users.noreply.github.com>
2025-09-03 10:07:46 +0200

    Update DASM.py

--- DASM.py
+++ DASM.py
@@ -1,27 +1,62 @@
+
+import pyperclip
+import keyboard
+
+
+
 MainROM = []
 ValueROM = []
 Tokens = []
 
-Registers = {"REG0":0, "REG1":1,"REG2":2, "REG3":3}
+REGISTERS = {"REG0":0, "REG1":1,"REG2":2, "REG3":3}
+CALCINSTR = {"ADD":0, "SUB":1, "MUL":2}
 
 def InterpretIMM(args) -> list:
-    return [0, args[0]]
+    return [0, int(args[0])]
 def InterpretMOV(args):
-    return [1<<12 + Registers[args[0]]<<8 + Registers[args[1]]<<4, 0]
+    return [(1<<12) + (REGISTERS[args[0].upper()]<<8) + (REGISTERS[args[1].upper()]<<4), 0]
 def InterpretCAL(args):
-    return []
+    return [(1<<12) + (REGISTERS[args[0].upper()]<<8) + (REGISTERS[args[1].upper()]<<4) + CALCINSTR[args[2].upper()], 0]
 
 
 with open("test.dasm") as f:
     lines = f.readlines()
     for line in lines:
         line = line.removesuffix("\n").split(" ")
+        val = []
         if line[0] == "IMM":
-            InterpretIMM(line[1:])
+            val = InterpretIMM(line[1:])
         elif line[0] == "MOV":
-            InterpretMOV(line[1:])
+            val = InterpretMOV(line[1:])
         elif line[0] == "CAl":
-            InterpretCAL(line[1:])
+            val = InterpretCAL(line[1:])
+        MainROM.append(val[0])
+        ValueROM.append(val[1])
+
+
+str0 = ""
+
+print(MainROM[1])
+print(ValueROM[1])
+for i in range(0, 256):
+    if (i < len(MainROM)):
+        str0 += format(MainROM[i], '16b') + "\n"
+    else:
+        str0+="0000000000000000\n"
+str1 = ""
+for i in range(0, 256):
+    if (i < len(ValueROM)):
+        str1 += format(ValueROM[i], '16b')  + "\n"
+    else:
+        str1+="0000000000000000\n"
+
+pyperclip.copy(str0)
+while not keyboard.is_pressed('a'):
+    pass
+pyperclip.copy(str1)
+    
+
+
 
 
         
