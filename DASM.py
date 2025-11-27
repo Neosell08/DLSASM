fe87dbab5201d37e18bd4bdbe3be39c65315e0b9
Neosell <141836762+Neosell08@users.noreply.github.com>
2025-11-27 08:23:06 +0100

    16bit address

--- DASM.py
+++ DASM.py
@@ -7,6 +7,8 @@ import keyboard
 MainROM = []
 ValueROM = []
 Tokens = []
+Macros = {}
+lines = []
 
 
 class Instruction:
@@ -31,20 +33,14 @@ def InterpretReadWriteMain(arg: str, r: bool): #interpret where to read and writ
 
     if r:
         if (arg.upper().startswith("RM")): #read from RAM
-            val[0] += 32
-            val[1] += int(arg.upper().removeprefix("RM"))<<8
-        elif (arg.upper().startswith("SC")): #read to screen
-            val[0] += 2
-            val[1] += int(arg.upper().removeprefix("SC"))
+            val[0] += 32 + ((int(arg.upper().removeprefix("RM"))&)<<8)
+            val[1] += 
         else:
             val[1] += REGISTERS[arg.upper()]
     else:       
         if (arg.upper().startswith("RM")): #write to RAM
             val[0] += 16
             val[1] += int(arg.upper().removeprefix("RM"))<<8
-        elif (arg.upper().startswith("SC")): #write from screen
-            val[0] += 3
-            val[1] += int(arg.upper().removeprefix("SC"))
         else:
             
             val[1] += REGISTERS[arg.upper()]
@@ -84,33 +80,69 @@ def InterpretCAL(args): #Calculate and move into REG0. First arg is A second is 
     return [2 + (REGISTERS[args[1].upper()]<<12) + (CALCINSTR[args[2].upper()]<<4), (REGISTERS[args[0].upper()]<<4)]
 def InterpretRSSC(args): #Reset Screen
     return [5, 0]
+
 def InterpretJMP(args): #Jump to line
-    print((int(args[0])|48))
-    return [3 + ((int(args[0])&48)>>4), int(args[0])&15]
+    args[0] = FindJumpLocation(args[0])
+    return [3 + ((args[0]&48)>>4), args[0]&15]
 def InterpretJMI(args):
-    return [6  + + ((int(args[0])&48)>>12) + (REGISTERS[args[1]]<<12), int(args[0])&15]
+    args[0] = FindJumpLocation(args[0])
+    return [6  + ((args[0]&48)>>12) + (REGISTERS[args[1]]<<12), args[0]&15]
+def FindJumpLocation(arg):
+    
+    if arg.upper() in Macros:
+        return Macros[arg.upper()]
+    else:
+        return int(arg)
+    
 def InterpretWRT(args): #Draw to Screen ex: WRT 
     val = [(4) + (REGISTERS[args[0]]<<12), 0]
     return val
 def InterpretRFSC(args): #Refresh Screen
     return [(3) + (8<<8), 0]
 
-INSTRSET = {"IMM":Instruction("IMM", 1, InterpretIMM), "MOV":Instruction("MOV", 2, InterpretMOV), "CAL":Instruction("CAL", 3, InterpretCAL), "RSSC":Instruction("RSSC", 0, InterpretRSSC), "JMP":Instruction("JMP", 1, InterpretJMP), "WRT":Instruction("WRT", 1, InterpretWRT), "JMI":Instruction("JMI", 2, InterpretJMI)}
+INSTRSET = {"IMM":Instruction("IMM", 1, InterpretIMM),
+            "MOV":Instruction("MOV", 2, InterpretMOV),
+            "CAL":Instruction("CAL", 3, InterpretCAL),
+            "RSSC":Instruction("RSSC", 0, InterpretRSSC),
+            "JMP":Instruction("JMP", 1, InterpretJMP),
+            "WRT":Instruction("WRT", 1, InterpretWRT),
+            "JMI":Instruction("JMI", 2, InterpretJMI)
+                       }
 
 
 with open("test.dasm") as f:
     lines = f.readlines()
-    for index in range(len(lines)):
-        line = lines[index].removesuffix("\n").upper().split(" ")
-        val = INSTRSET[line[0].upper()].call(line[1:], index) #line[0] = instr, line[1:] = args
+    
+    i = 0
+    #preprocessing
+    while i < len(lines):
+        lines[i] = lines[i].split("#")[0].removesuffix(" ").removesuffix("\n").upper()
+        if (lines[i].split(" ")[0] == ""):
+            lines.pop(i)
+            i -= 1
+        if (lines[i].endswith(":")):
+            Macros[lines[i].removesuffix(":")] = i - len(Macros)
+            lines.pop(i)
+            i -= 1
+        i += 1
+    i = 0
+    while i < len(lines):
+        line = lines[i].removesuffix("\n").split(" ")
+        if line[0] == "":
+            i += 1
+            continue
+        val = INSTRSET[line[0].upper()].call(line[1:], i) #line[0] = instr, line[1:] = args
         MainROM.append(val[0])
         ValueROM.append(val[1])
+        i += 1
 
 
 str0 = ""
 
+print("written to code file")
+
+
 for i in range(0, len(MainROM)):
-    
    str0 += str(format(MainROM[i] + (ValueROM[i] << 16), f'0{8}x')) + " "
 try:
     with open("Circuits/code", "x") as f:
--- DASMP.py
+++ DASMP.py
@@ -1,1 +1,26 @@
-wrt
+lines = []
+
+with open("dap.dasmp", "r") as f:
+    lines = f.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].removesuffix("\n")
+
+class Instruction:
+    def __init__(self, keyword: str, params: int, func):
+        self.keyword = keyword
+        self.params = params
+        self.func = func
+    def call(self, args: list, linenum):
+        if (len(args) != self.params):
+            raise ValueError("Not enough paramaters in function: " + self.keyword + " at line " + str(linenum) + ". " + str(len(args)) + " given.\nArgs: " + str(args))
+        return self.func(args)
+
+def InterpretWrite(args):
+    lines = []
+    for c in args[0]:
+        lines.append("IMM " + str(ord(c)))
+        lines.append("WRT REG0")
+    
+    
+
+INSTRSET = {"write":Instruction("write", 1, InterpretWrite)}
